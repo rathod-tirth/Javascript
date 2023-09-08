c68f8cc1ce9720c3129399d9109066687c16230d
rathod-tirth <tirthrathod6@gmail.com>
2023-09-08 22:29:34 +0530

    JS

--- Vanilla/40asynchronous.js
+++ Vanilla/40asynchronous.js
@@ -2,7 +2,7 @@
 console.log("Synchronous 1");
 
 // Asynchronous macrotask (gets called after the start of a new event loop)
-setTimeout(() => console.log("Timeout 2"),0);
+setTimeout(() => console.log("Timeout 2"), 0);
 
 // Asynchronous microtask (gets called before the start of a new event loop )
 Promise.resolve().then(() => console.log("Promise 3"));
